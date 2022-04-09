cb68cedf8b071e7354c9cefe5f9a771463affa83
Facundo Coronel <coronelfacundo30@gmail.com>
2022-04-09 15:19:28 -0300

    Fin diseño registro

--- frontend/src/app/components/company-register/company-register.component.ts
+++ frontend/src/app/components/company-register/company-register.component.ts
@@ -7,9 +7,47 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CompanyRegisterComponent implements OnInit {
 
+  //Datos de la empresa
+  email!: string;
+  razonSocial!: string;
+  phone!: string;
+  description!: string;
+  password!: string;
+  tpassword!: string
+
+  //Ubicación de las oficinas
+  country!: string;
+  state!: string;
+  locality!: string;
+  code!: string;
+  street!: string;
+  by!: string;
+
+  //Razón de la solicitud, términos y condiciones, y verificar password
+  checkpass!: boolean
+  application!: string;
+  terminos: boolean = false
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  equalpass(){
+    if(this.password===this.tpassword){
+      this.checkpass = true
+    }else{
+      this.checkpass = false
+    }
+  }
+
+  checkterminos(){
+    if(this.terminos==false){
+       this.terminos = true
+    }else{
+      this.terminos = false
+    }
+    console.log(this.terminos)
   }
 
 }
