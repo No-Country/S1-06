7dfdd95eeefc363d7fdb85999e6e88c3c2ae2608
noel toyo <=>
2022-04-13 13:08:00 -0400

    [ADD] added cors

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -27,6 +27,9 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:4200',
+]
 
 # Application definition
 
@@ -39,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'drf_yasg',
+    'corsheaders',
     'users',
     'recruiter',
     'applicant',
