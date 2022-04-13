dec05c932184a4bdfe0afeef47feb95efe54bcf5
noel toyo <=>
2022-04-13 13:09:16 -0400

    [ADD] added custom token serializer

--- backend/users/api/serializers.py
+++ backend/users/api/serializers.py
@@ -1,9 +1,27 @@
+from django.contrib.auth import get_user_model
 from rest_framework import serializers
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from users.models import Location 
+from applicant.api.serializers import ApplicantProfileSerializer
+from recruiter.api.serializers import RecruiterProfileSerializer
 
-from users.models import Location
-
-
-class LocationSerializer(serializers.ModelSerializer):
+class UserSerializer(serializers.ModelSerializer):
     class Meta:
-        model = Location
-        fields = ('country', 'city', 'postal_code')
+        model = get_user_model()
+        fields = ('id', 'email', 'is_superuser', 'is_applicant', 'is_recruiter')
+
+class CustomTokenObtainPairSerializer(TokenObtainPairSerializer):
+    def validate(self, attrs):
+        data = super(CustomTokenObtainPairSerializer, self).validate(attrs)
+        data.update({'user': UserSerializer(instance=self.user).data})
+        profile = {}
+        if (self.user.is_applicant):
+            profile = ApplicantProfileSerializer(
+                instance=self.user.applicant_profile
+            ).data
+        elif (self.user.is_recruiter):
+            profile = RecruiterProfileSerializer(
+                instance=self.user.applicant_profile
+            ).data
+        data.update({ 'profile': profile })
+        return data
