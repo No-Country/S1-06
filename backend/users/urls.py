b26964e57bdf996f54acf18427a8b54c5d9f41ea
noel toyo <=>
2022-04-13 13:06:07 -0400

    [ADD] added custom token

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -1,13 +1,13 @@
 from django.urls import path
 from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
     TokenRefreshView,
 )
+from .api.views import MyTokenObtainPairView
 
 app_name = 'users'
 
 urlpatterns = [
-    path('auth/token/', TokenObtainPairView.as_view(), name='token'),
+    path('auth/token/', MyTokenObtainPairView.as_view(), name='token'),
     path(
         'auth/token/refresh/',
         TokenRefreshView.as_view(),
