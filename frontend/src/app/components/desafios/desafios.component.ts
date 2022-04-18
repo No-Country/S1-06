532ecf198f3de75ecf83348fbd421ad922271612
noel toyo <=>
2022-04-18 11:33:07 -0400

    [ADD] show button create challenge only if is recruiter or admin

--- frontend/src/app/components/desafios/desafios.component.ts
+++ frontend/src/app/components/desafios/desafios.component.ts
@@ -1,3 +1,4 @@
+import { AuthService } from '../../services/auth.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,9 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DesafiosComponent implements OnInit {
 
-  constructor() { }
+  isNotApplicant:boolean = false
+  constructor(private authService: AuthService) { }
 
-  ngOnInit(): void {
+  ngOnInit (): void {
+    this.isNotApplicant = ['admin', 'recruiter'].includes(this.authService.getTypeUser())
   }
 
 }
