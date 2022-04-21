9b8265011ef73c204e162ebbadeffded7ecc3a57
noel toyo <=>
2022-04-21 12:15:38 -0400

    [ADD] added router params

--- frontend/src/app/components/desafios/desafios.component.ts
+++ frontend/src/app/components/desafios/desafios.component.ts
@@ -1,5 +1,6 @@
 import { AuthService } from '../../services/auth.service';
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-desafios',
@@ -8,11 +9,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DesafiosComponent implements OnInit {
 
-  isNotApplicant:boolean = false
-  constructor(private authService: AuthService) { }
+  isNotApplicant: boolean = false
+  challenge_id:string = "0"
+  constructor(private authService: AuthService, private _Activatedroute:ActivatedRoute) { }
 
   ngOnInit (): void {
     this.isNotApplicant = ['admin', 'recruiter'].includes(this.authService.getTypeUser())
+    this._Activatedroute.paramMap.subscribe(params => {
+      this.challenge_id = params.get('challenge_id') ?? "";
+    });
   }
 
 }
