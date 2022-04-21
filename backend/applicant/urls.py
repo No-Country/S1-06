6a972f295b5b37ddf5cc4664ee3f60cbff3feeae
noel toyo <=>
2022-04-21 12:37:07 -0400

    [ADD] added complete challenge route

--- backend/applicant/urls.py
+++ backend/applicant/urls.py
@@ -1,3 +1,5 @@
+from django.urls import path
+
 from rest_framework.routers import DefaultRouter
 
 from .api.views import ApplicantUserViewSet
@@ -8,4 +10,21 @@ router = DefaultRouter()
 router.register(r'applicant', ApplicantUserViewSet, basename='applicant')
 urlpatterns = router.urls
 
-urlpatterns += []
+urlpatterns += [
+    path(
+        "applicant/<int:pk>/challenge/<int:challenge_id>/",
+        ApplicantUserViewSet.as_view(
+            {'post': 'complete_challenge', 'get': 'get_challenge'}
+        ), name="applicant_challenge"
+    ),
+    path(
+        "applicant/challenge/<int:challenge_id>/verify/",
+        ApplicantUserViewSet.as_view(
+            {'get': 'verify_challenge_complete'}
+        ), name="applicant_challenge_verify"
+    ),
+]
+
+
+
+
