1c66ffc825c247df89805c25a512b05a23972ff9
noel toyo <=>
2022-04-21 12:19:57 -0400

    [ADD] added question component

---
+++ frontend/src/app/components/question/question.component.ts
@@ -0,0 +1,28 @@
+import { QuestionService } from './../../services/question.service';
+import { Component, Input, OnInit } from '@angular/core';
+import { KeyValueChanges, KeyValueDiffer, KeyValueDiffers } from '@angular/core';
+
+@Component({
+  selector: 'app-question',
+  templateUrl: './question.component.html',
+  styleUrls: ['./question.component.css']
+})
+export class QuestionComponent implements OnInit {
+  @Input()data:any;
+  @Input()index:any;
+
+  da: any;
+
+  constructor(private questionService: QuestionService) { }
+
+  ngOnInit(): void {
+  }
+
+  onChangeEvent (event: any) {
+    const payload = {choice: event.target.value, question: this.data.id}
+    this.questionService.questionDispatch.emit({
+      ...payload
+    })
+  }
+
+}
