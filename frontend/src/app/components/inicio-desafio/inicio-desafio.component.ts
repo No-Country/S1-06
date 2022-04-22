e61f4aa31c38c670215b01d470265097f94ecb26
noel toyo <=>
2022-04-22 13:34:32 -0400

    [ADD] added validation if user made the challenge

--- frontend/src/app/components/inicio-desafio/inicio-desafio.component.ts
+++ frontend/src/app/components/inicio-desafio/inicio-desafio.component.ts
@@ -1,6 +1,8 @@
+import { ApplicantService } from './../../services/applicant.service';
 import { AuthService } from '../../services/auth.service';
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-inicio-desafio',
@@ -12,11 +14,33 @@ export class InicioDesafioComponent implements OnInit {
   isApplicant: boolean = false
   challenge_id:string = "0"
 
-  constructor(private authService: AuthService, private _Activatedroute:ActivatedRoute) { }
+  constructor(
+    private authService: AuthService,
+    private _Activatedroute: ActivatedRoute,
+    private applicanteService: ApplicantService,
+    private router: Router
+  ) { }
   ngOnInit (): void {
     this.isApplicant = this.authService.getTypeUser() === 'applicant' ? true : false;
     this._Activatedroute.paramMap.subscribe(params => {
       this.challenge_id = params.get('challenge_id') ?? "";
     });
+  }
+  goToChallenge () {
+    this.applicanteService.verifyChallenge(this.challenge_id)
+      .subscribe((data: any) => {
+        if (data.exits) {
+          this.swalError('Este desafio ya ha sido realizado')
+        } else {
+          this.router.navigate(['/jugar-desafio', this.challenge_id]);
+        }
+       });
+  }
+  swalError (text: string) {
+    Swal.fire({
+      title: 'Error',
+      icon: 'error',
+      text: text,
+    })
   }
 }
