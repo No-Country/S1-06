b21e192ed67cf26fc5500b85b9a576f858c54a85
noel toyo <=>
2022-04-22 13:22:11 -0400

    [ADD] added applicant answers and get all challenges

--- backend/applicant/urls.py
+++ backend/applicant/urls.py
@@ -23,8 +23,17 @@ urlpatterns += [
             {'get': 'verify_challenge_complete'}
         ), name="applicant_challenge_verify"
     ),
+    path(
+        "applicant/<int:pk>/challenges/",
+        ApplicantUserViewSet.as_view(
+            {'get': 'get_all_challenges'}
+        ), name="applicant_all_challenge"
+    ),
+    path(
+        "applicant/<int:pk>/challenge/<int:challenge_id>/answers/",
+        ApplicantUserViewSet.as_view(
+            {'get': 'get_challenge_answers'}
+        ), name="applicant_answers"
+    ),
 ]
-
-
-
 
