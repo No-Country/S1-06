1b4b5fc1bcb381728dec837234427d84332458cc
noel toyo <=>
2022-04-22 13:19:05 -0400

    [ADD] added get challenges recruiter

--- backend/recruiter/api/views.py
+++ backend/recruiter/api/views.py
@@ -1,8 +1,17 @@
 from django.contrib.auth import get_user_model
-from rest_framework import viewsets
+from rest_framework import viewsets, status
+from rest_framework.decorators import action
+from rest_framework.response import Response
 from .serializers import RecruiterUserSerializer
 
+from challenge.api.serializers import ChallengeSerializer
 
 class RecruiterUserViewSet(viewsets.ModelViewSet):
     serializer_class = RecruiterUserSerializer
     queryset = get_user_model().recruiter.all()
+
+    @action(methods=['GET'], detail=True)
+    def challenges(self, request, pk):
+        challenges = self.get_object().challenges.all()
+        serializer = ChallengeSerializer(challenges, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
