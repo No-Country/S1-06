dbb56603a16a6137ea9e50739cbaed4084841602
noel toyo <=>
2022-04-22 13:18:33 -0400

    [ADD] added new fields

--- backend/recruiter/models.py
+++ backend/recruiter/models.py
@@ -1,10 +1,13 @@
 from django.db import models
 from django.contrib.auth import get_user_model
-from users.models import Profilable, Location
+from users.models import Location
 
 
-class RecruiterProfile(Profilable, models.Model):
+class RecruiterProfile(models.Model):
     name = models.CharField(max_length=100)
+    request_reason = models.CharField(default="", max_length=255, blank=True)
+    phone = models.CharField(max_length=100, blank=True, null=True)
+    description = models.CharField(max_length=255, blank=True, default="")
     user = models.OneToOneField(
         get_user_model(),
         related_name='recruiter_profile',
