6c2f73a0e765896f6aceefcd6236c10b6566f80a
noel toyo <=>
2022-04-22 13:22:45 -0400

    [ADD] added applicant answers and get all challenges

--- backend/applicant/api/views.py
+++ backend/applicant/api/views.py
@@ -2,20 +2,28 @@ from django.contrib.auth import get_user_model
 from rest_framework import viewsets, status
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
+from drf_yasg.utils import swagger_auto_schema
 from .serializers import ApplicantUserSerializer
-from challenge.api.serializers import ChallengeCompleteSerializer
-from challenge.models import ChallengeComplete
+from challenge.api.serializers import ChallengeCompleteSerializer, \
+    ChallengesApplicantSerializer, AnswerCreateSerializer
+from challenge.models import ChallengeComplete, Answer
 
 class ApplicantUserViewSet(viewsets.ModelViewSet):
     serializer_class = ApplicantUserSerializer
     queryset = get_user_model().applicant.all()
 
+    @swagger_auto_schema(responses={200: ChallengesApplicantSerializer})
     def get_challenge(self, pk, challenge_id):
         challenge = ChallengeComplete.objects.filter(
             applicant_id=pk,
             challenge_id=challenge_id
         )
-        serializer = ChallengeCompleteSerializer(challenge)
+        serializer = ChallengesApplicantSerializer(challenge)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def get_all_challenges(self, request, pk):
+        challenge = ChallengeComplete.objects.filter(applicant_id=pk)
+        serializer = ChallengesApplicantSerializer(challenge, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
 
     def complete_challenge(self, request, pk, challenge_id):
@@ -35,3 +43,11 @@ class ApplicantUserViewSet(viewsets.ModelViewSet):
             challenge_id=challenge_id
         ).all()
         return Response({"exits": challenge.exists()}, status=status.HTTP_200_OK)
+
+    def get_challenge_answers(self, requests, pk, challenge_id):
+        answers = Answer.objects.filter(
+            challenge_applicant__applicant=pk,
+            challenge_applicant__challenge__id=challenge_id
+        )
+        serializer = AnswerCreateSerializer(answers, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
