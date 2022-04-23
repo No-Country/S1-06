c0470456525c7b5e2c983932fecd24372c08e666
noel toyo <=>
2022-04-23 00:09:11 -0400

    [ADD][DEL] added user name and deleted mis desafios

--- frontend/src/app/components/navbar/navbar.component.ts
+++ frontend/src/app/components/navbar/navbar.component.ts
@@ -9,13 +9,31 @@ import { AuthService } from '../../services/auth.service';
 })
 export class NavbarComponent implements OnInit {
 
+  name:string = ''
+
   constructor(private authService: AuthService,  private router: Router) { }
 
-  ngOnInit(): void {
+  ngOnInit (): void {
+    const typeUser = this.authService.getTypeUser()
+    const currentUser = this.authService.getCurrentUser()
+    this.name = this.getUserName(currentUser)[typeUser]() ?? 'Usuario'
   }
 
   cerrarsesion(){
     this.authService.logout()
     this.router.navigate(['/login'])
+  }
+  getUserName (user: any): any {
+    return {
+      recruiter () {
+        return user.profile.name
+      },
+      applicant () {
+        return `${user.profile.first_name} ${user.profile.last_name}`
+      },
+      admin () {
+        return 'admin'
+      }
+    }
   }
 }
